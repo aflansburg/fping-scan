100570056674fdc9ff81e755bacf0be7c7a2a6a0
Abram Flansburg <abe.flansburg@gmail.com>
2016-04-06 14:47:47 -0500

    .py initial file commit -- usage of fping in this instance

---
+++ fping-scan.py
@@ -0,0 +1,14 @@
+__author__ = 'misaflansb'
+
+'''
+
+fping -a -q -g <ip_addr>
+           -a = alive hosts
+           -q = quiet
+           -g = address + mask (for scanning subnets)
+
+'''
+
+import os
+
+
