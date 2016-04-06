e7645d7cb428d38dfc0db55ee6122481f766c7fc
Abram Flansburg <abe.flansburg@gmail.com>
2016-04-06 16:06:07 -0500

    That's working -- need to run this on every subnet -- compare with dhcpd.leases file and whatever isn't listed there should be a static assignment set on the device itself.

--- fping-scan.py
+++ fping-scan.py
@@ -14,12 +14,18 @@ import time # this is for timing the subnet ping scan
 
 addr_list = []
 
+# For command counter
 start = time.time()
 
 for addr in os.popen("fping -a -q -g 10.31.94.0/23"):
-    if addr != '\n':
-        addr_list.append(addr)
-        print addr
+    addr = addr.rstrip('\n')
+    addr_list.append(addr)
+    print addr
 
+# Simple counter to calculate time to find up hosts
 end = time.time()
+start = int(start)
+end = int(end)
+
 print ("Time elapsed: {0}".format(end-start) + " seconds")
+print ("Total number of hosts up in this subnet: {0}".format(len(addr_list)))
